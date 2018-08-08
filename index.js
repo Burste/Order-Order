06c3c2e3a9e9b52f7ed9a1d29c5d6263648eff85
felix <oxipn1290@gmail.com>
2018-08-08 17:25:03 +0800

    add linebot reply

--- index.js
+++ index.js
@@ -7,8 +7,35 @@ var bot = linebot({
     channelAccessToken: 'ZKsloqKahDnLX5D218JXiK6k2k6pRAaKm10bHQiXXQWWbAmQAyDYMcir2gPqbfvCChXsQEWQr4uPphvpcA8J1lOWnMcsB9LlgvurrJIUh74AOGxz2IUzmphHjITT1YK2/PeLGw3vftInfqYZ6qaWdgdB04t89/1O/w1cDnyilFU='
 });
 
+const commandList = [
+    { cmd: "order start", msg: "------ 開始點餐 ------" },
+    { cmd: "order end", msg: "------ 點餐結束 ------" },
+    { cmd: "order list", msg: "------ 點餐明細 ------" },
+];
+
+var commandMode = function(cmd) {
+    var filterItem = commandList.filter(function(item) {
+        return item.cmd === cmd;
+    });
+    return filterItem[0].msg;
+};
+
 bot.on('message', function(event) {
     console.log(event); //把收到訊息的 event 印出來看看
+    var replyMsg = "";
+    if(event.message.type = 'text') {
+        var msg = event.message.text;
+        if(Object.keys(commandList).indexOf(msg) !== -1) {
+            replyMsg = commandMode(msg);
+        }
+    }
+    event.reply(replyMsg).then(function(data) {
+        // success
+        console.log(replyMsg);
+    }).catch(function(error) {
+        // error
+        console.log('error');
+    });
 });
 
 const app = express();
